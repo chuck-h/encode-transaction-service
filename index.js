abb3c2c8ca4174432cb19c49be599517a23af62c
chuck <cfharr@gmail.com>
2023-04-30 12:50:10 -0700

    restore port 3000

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ fastify.get('/invoice', async (request, reply) => {
 
 const start = async () => {
     try {
-        await fastify.listen(3010) 
+        await fastify.listen(3000) 
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
